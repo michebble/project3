71339ac8a1427ba08c791a5bbc1b6a8909520718
Franzzisco <franzzisco@gmail.com>
2018-05-08 09:41:42 +1000

    Edit Controllers and View with Dummy Data

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,35 @@
 class UsersController < ApplicationController
 
+  #
+  # def index
+  #   @users = User.all.order('created_at DESC')
+  # end
+  #
+  # def new
+  #   # @user = User.new
+  # end
+  #
+  # def create
+  #   @user = User.new(post_params)
+  #   if @user.save
+  #     redirect_to @user
+  #   else
+  #     render 'new'
+  #   end
+  # end
+  #
+  # def show
+  #   @user = User.find(params[:id])
+  # end
 
+  def index
+    # this can be changed in the future
+    @search = Search.all.order('created_at DESC')
+  end
+
+  def show
+    @user = User.find(params[:id])
+    @search = Search.all.order('created_at DESC')
+  end
 
 end
