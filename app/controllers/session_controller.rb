5098905c8bc4a58af6a0456b374039270a0fe889
Devatanu Deka <deva.deka@gmail.com>
2018-05-08 12:35:59 +1000

    return user json

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -3,13 +3,35 @@
 class SessionController < ApplicationController
 
     def new
-        p "ENV::::::#{ENV['CLIENT_ID']}"
-        @spotify_service = "https://accounts.spotify.com/authorize/?client_id=#{ENV['CLIENT_ID']}&response_type=code&scope=user-read-private&redirect_uri=http://localhost:3000/callback/"
+        @spotify_service = "https://accounts.spotify.com/authorize/?client_id=#{ENV['CLIENT_ID']}&response_type=code&scope=user-read-private%20user-read-currently-playing&redirect_uri=http://localhost:3000/callback/"
     end
 
     def spotify_callback
-        # access_token = params[:access_token]
-        p params
+        access_response = HTTParty.post("https://accounts.spotify.com/api/token",
+            :query => { 
+                :grant_type => "authorization_code",
+                :code => params[:code],
+                :redirect_uri => "http://localhost:3000/callback/",
+                :client_id => ENV['CLIENT_ID'],
+                :client_secret => ENV['CLIENT_SECRET']
+                })
+        if access_response.keys.include? "error"
+            return "NO"
+        else
+            user_response = HTTParty.get("https://api.spotify.com/v1/me",
+                :query => { 
+                    :access_token => access_response["access_token"]
+                    })
+            render json: user_response
+            # raise 'ERROR'
+        end
+
+        # login_response = HTTParty.get("https://api.spotify.com/v1/me/player/currently-playing",
+        #     :query => { 
+        #         :access_token => access_response["access_token"]
+        #         })
+        
+
     end
 
     def create
