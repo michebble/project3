054c205ae1f5495d6d2ba0540d80c714d2ffafa6
Franzzisco <franzzisco@gmail.com>
2018-05-09 08:48:13 +1000

    Dummydata for new tables -displayed DB in chatroom -new seed file

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,7 +1,10 @@
 class RoomsController < ApplicationController
+
   def show
+    params[:id] = 1
     @room_id = params[:id]
     @messages = Message.where(conversation_id: params[:id])
+    @messages = Message.all.order('created_at DESC')
 
   end
   def new
@@ -12,13 +15,14 @@ class RoomsController < ApplicationController
 
   def update
     message = Message.new
-    message.user_id = user.spotify_id
+    message.user_id = user.spotify_id?
     message.conversation_id = params[conversation_id]
     message.content = params["message"]
 
     message.save
 
     # need to add room id
+    # room id would be the conversation_id given the moment they matched and allocated it to the participant table where both users share that conversation_id
   end
 
 end
