3b97e61e0aa12f1bffa46fafee0b877918882fbf
Devatanu Deka <deva.deka@gmail.com>
2018-05-08 21:22:49 +1000

    user can go frmo login to their profile to match, match page will show what song they are listenting to currently on spotify - name and album art

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,2 +1,35 @@
 class SearchesController < ApplicationController
+
+
+  def new
+  
+    #HTTPARTY
+    @song_response = HTTParty.get("https://api.spotify.com/v1/me/player/currently-playing",
+      :query => { 
+          :access_token => session[:access_token]
+          })
+    
+    
+  
+  end
+
+
+  def find_match
+
+    
+    user = User.find_by(spotify_id: session[:spotify_id])
+    search = Search.new
+    search.song_name
+    
+    raise ""
+    #DB CREATE
+    
+
+    #DB QUERY
+
+
+    #REDIRECT MATCH SHOW
+
+
+  end
 end
