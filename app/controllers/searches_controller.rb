a34a4755ef040c2d6f7a7bff2f6371ba764ba120
Franzzisco <franzzisco@gmail.com>
2018-05-09 10:44:07 +1000

    changes on the controller

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -2,22 +2,22 @@ class SearchesController < ApplicationController
 
 
   def new
-  
+
     #HTTPARTY
     @song_response = HTTParty.get("https://api.spotify.com/v1/me/player/currently-playing",
-      :query => { 
+      :query => {
           :access_token => session[:access_token]
           })
     if @song_response.code == 200
       @song_id = @song_response['item']['id']
       @song_name = @song_response['item']['name']
-      @song_artist = @song_response['item']['artists'].count>1 ? "#{@song_response['item']['artists'][0]['name']} and others" :  "#{@song_response['item']['artists'][0]['name']}" 
+      @song_artist = @song_response['item']['artists'].count>1 ? "#{@song_response['item']['artists'][0]['name']} and others" :  "#{@song_response['item']['artists'][0]['name']}"
       @album_image_url = @song_response['item']['album']['images'][2]['url']
       @album_name = @song_response['item']['album']['name']
 
       @match_url = "/match?song_id=#{@song_id}&song_name=#{@song_name}&artist=#{@song_artist}&img_url=#{@album_image_url}&album=#{@album_name}"
-    
-      
+
+
     end
 
   end
@@ -25,7 +25,7 @@ class SearchesController < ApplicationController
 
   def find_match
 
-    
+
     @user = User.find_by(spotify_id: session[:spotify_id])
     search = Search.new
     search.song_id = params['song_id']
@@ -41,13 +41,13 @@ class SearchesController < ApplicationController
 
     #REDIRECT MATCH SHOW
     match_search = Search.where(:song_id => Search.last.song_id).where.not(:user_id => User.last.id)
-    if !!match_search
+    if match_search.count>0
       @match_user = match_search.sample.user
     else
       @match_user = nil
     end
 
-    
+
     render :match
 
 
