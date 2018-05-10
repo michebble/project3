53498d5f821dc4c40e0d70cda8bc7a7eed347f57
Franzzisco <franzzisco@gmail.com>
2018-05-10 12:07:13 +1000

    Search Controller update getting ajax matched

--- app/controllers/api/searches_controller.rb
+++ app/controllers/api/searches_controller.rb
@@ -1,36 +1,32 @@
 class Api::SearchesController < ApplicationController
-  
+
   def find_match
-
-
-   
-
-    @user = User.find(session[:user_id])
-    @search = Search.new
-    @search.song_id = params['song_id']
-    @search.song_name = params['song_name']
-    @search.artist = params['artist']
-    @search.img_url = params['img_url']
-    @search.album = params['album']
-    @search.user_id = @user.id
-    @search.save
+    user = User.find(session[:user_id])
+    search = Search.new
+    search.song_id = params['song_id']
+    search.song_name = params['song_name']
+    search.artist = params['artist']
+    search.img_url = params['img_url']
+    search.album = params['album']
+    search.user_id = user.id
+    search.save
 
     #DB QUERY
-    @search_matches = Search.where(:song_id => @search.song_id).where.not(:user_id => @user.id).where("created_at > ?", 5.minutes.ago)
-    if @search_matches.count > 0
+    search_matches = Search.where(:song_id => search.song_id).where.not(:user_id => user.id).where("created_at > ?", 5.minutes.ago)
+    if search_matches.count > 0
       matched_search = search_matches.sample
-      @match_user = matched_search.user
-      conversation_url = create_room(@user, @match_user, matched_search)
-      result = {"match_user" => match_user, "conversation_url" => conversation_url} 
+      match_user = matched_search.user
+      conversation_url = create_room(user, match_user, matched_search)
+      result = {"match_user" => match_user, "conversation_url" => conversation_url}
     else
-      result = {"match_user" => nil, "conversation_url" => nil} 
+      result = {"match_user" => nil, "conversation_url" => nil}
     end
     result.to_json
     render json: result
 
   end
-  
-  def create_room(user1, user2, search) 
+
+  def create_room(user1, user2, search)
     conversation = Conversation.new
     conversation.song_id = search.song_id
     conversation.song_name = search.song_name
@@ -42,7 +38,7 @@ class Api::SearchesController < ApplicationController
     participant1.user = user1
     participant1.conversation = conversation
     participant1.save
-    
+
     participant2 = Participant.new
     participant2.user = user2
     participant2.conversation = conversation
