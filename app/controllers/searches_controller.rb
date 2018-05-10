41c6c1c3c747be7694b1473005ec006fd58a1510
Michael Hebblethwaite <mhhebble@gmail.com>
2018-05-10 13:25:56 +1000

    searches controller

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -23,59 +23,53 @@ class SearchesController < ApplicationController
   end
 
 
-  def find_match
+  # def find_match
 
    
-    @user = User.find_by(spotify_id: session[:spotify_id])
-    search = Search.new
-    search.song_id = params['song_id']
-    search.song_name = params['song_name']
-    search.artist = params['artist']
-    search.img_url = params['img_url']
-    search.album = params['album']
-    search.user_id = @user.id
-    search.save
+  #   @user = User.find_by(spotify_id: session[:spotify_id])
+  #   search = Search.new
+  #   search.song_id = params['song_id']
+  #   search.song_name = params['song_name']
+  #   search.artist = params['artist']
+  #   search.img_url = params['img_url']
+  #   search.album = params['album']
+  #   search.user_id = @user.id
+  #   search.save
 
-    #DB QUERY
-    search_matches = Search.where(:song_id => search.song_id).where.not(:user_id => @user.id).where("created_at > ?", 5.minutes.ago)
-    if search_matches.count > 0
-      matched_search = search_matches.sample
-      @match_user = matched_search.user
-      @conversation_url = create_room(@user,@match_user,matched_search)
-      render :match
-    else
-      @match_user = nil
-      redirect_to '/searches/try_again'
-    end
-
-    #REDIRECT MATCH SHOW
-
-
-  end
-
-  def try_again
-    sleep(5)
-    redirect_to '/searches/new'
-  end
-
-  def create_room(user1, user2, search) 
-    conversation = Conversation.new
-    conversation.song_id = search.song_id
-    conversation.song_name = search.song_name
-    conversation.album_img_url = search.img_url
-    conversation.artist = search.artist
-    conversation.save
-
-    participant1 = Participant.new
-    participant1.user = user1
-    participant1.conversation = conversation
-    participant1.save
+  #   #DB QUERY
+  #   search_matches = Search.where(:song_id => search.song_id).where.not(:user_id => @user.id).where("created_at > ?", 5.minutes.ago)
+  #   if search_matches.count > 0
+  #     matched_search = search_matches.sample
+  #     @match_user = matched_search.user
+  #     @conversation_url = create_room(@user,@match_user,matched_search)
+  #     render :match
+  #   else
+  #     @match_user = nil
+  #     redirect_to '/searches/new'
+  #   end
     
-    participant2 = Participant.new
-    participant2.user = user2
-    participant2.conversation = conversation
-    participant2.save
 
-    return "/room?room_id=#{conversation.id}"
-  end
+  # end
+
+
+  # def create_room(user1, user2, search) 
+  #   conversation = Conversation.new
+  #   conversation.song_id = search.song_id
+  #   conversation.song_name = search.song_name
+  #   conversation.album_img_url = search.img_url
+  #   conversation.artist = search.artist
+  #   conversation.save
+
+  #   participant1 = Participant.new
+  #   participant1.user = user1
+  #   participant1.conversation = conversation
+  #   participant1.save
+    
+  #   participant2 = Participant.new
+  #   participant2.user = user2
+  #   participant2.conversation = conversation
+  #   participant2.save
+
+  #   return "/room?room_id=#{conversation.id}"
+  # end
 end
