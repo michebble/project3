16a023044781347124480b4c39f043c45f56f1f4
Franzzisco <franzzisco@gmail.com>
2018-05-10 15:33:41 +1000

    Matilda changes on my css

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -23,33 +23,7 @@ class SearchesController < ApplicationController
 
   # def find_match
 
-<<<<<<< HEAD
 
-    @user = User.find_by(spotify_id: session[:spotify_id])
-    search = Search.new
-    search.song_id = params['song_id']
-    search.song_name = params['song_name']
-    search.artist = params['artist']
-    search.img_url = params['img_url']
-    search.album = params['album']
-    search.user_id = @user.id
-    search.save
-
-    #DB QUERY
-    search_matches = Search.where(:song_id => search.song_id).where.not(:user_id => @user.id).where("created_at > ?", 5.minutes.ago)
-    if search_matches.count > 0
-      matched_search = search_matches.sample
-      @match_user = matched_search.user
-      @conversation_url = create_room(@user,@match_user,matched_search)
-      render :match
-    else
-      @match_user = nil
-      redirect_to '/searches/try_again'
-    end
-
-    #REDIRECT MATCH SHOW
-=======
-   
   #   @user = User.find_by(spotify_id: session[:spotify_id])
   #   search = Search.new
   #   search.song_id = params['song_id']
@@ -71,44 +45,12 @@ class SearchesController < ApplicationController
   #     @match_user = nil
   #     redirect_to '/searches/new'
   #   end
-    
->>>>>>> master
+
 
   # end
 
 
-<<<<<<< HEAD
-  def show_match
-    @user = User.find(session[:user_id])
-    @match_user = User.find(params[:match_user_id])
-    @conversation_url = params[:conversation_url]
-    render :match
-  end
-
-  def try_again
-    sleep(5)
-    redirect_to '/searches/new'
-  end
-
-  def create_room(user1, user2, search)
-    conversation = Conversation.new
-    conversation.song_id = search.song_id
-    conversation.song_name = search.song_name
-    conversation.album_img_url = search.img_url
-    conversation.artist = search.artist
-    conversation.save
-
-    participant1 = Participant.new
-    participant1.user = user1
-    participant1.conversation = conversation
-    participant1.save
-
-    participant2 = Participant.new
-    participant2.user = user2
-    participant2.conversation = conversation
-    participant2.save
-=======
-  # def create_room(user1, user2, search) 
+  # def create_room(user1, user2, search)
   #   conversation = Conversation.new
   #   conversation.song_id = search.song_id
   #   conversation.song_name = search.song_name
@@ -120,12 +62,11 @@ class SearchesController < ApplicationController
   #   participant1.user = user1
   #   participant1.conversation = conversation
   #   participant1.save
-    
+
   #   participant2 = Participant.new
   #   participant2.user = user2
   #   participant2.conversation = conversation
   #   participant2.save
->>>>>>> master
 
   #   return "/room?room_id=#{conversation.id}"
   # end
